61276f3ac5e523663a0c5044db36525f59f34949
William Alefe <53325812+williamaleff@users.noreply.github.com>
2024-06-12 17:25:28 -0300

    Update main.js
    Alteracao de project

--- diadosnamorados/main.js
+++ diadosnamorados/main.js
@@ -1,154 +1,70 @@
-//Variables
-let mobile_media_query = window.matchMedia("(max-width: 400px)");
-let tablet_media_query = window.matchMedia(
-  "(min-width: 400px) and (max-width: 600px)"
-);
-const notes = document.querySelectorAll(".js-note");
-
-//-> Function that resets the size of the notes.
-function recize_notes() {
-  for (let i = 0; i < notes.length; i++) {
-    if (notes[i].classList.contains("active")) {
-      notes[i].classList.remove("active");
-      gsap.set(notes[i], {
-        height: "30%",
-        clearProps: "all"
-      });
-    }
-  }
-}
-
-//-> Main function that enables all the notes.
-function notes_ready() {
-  gsap.to(".js-envelop-content", {
-    height: "110%",
-    duration: 0.5
-  });
-
-  for (let i = 0; i < notes.length; i++) {
-    notes[i].addEventListener("click", function () {
-      if (mobile_media_query.matches) {
-        if (this.classList.contains("active")) {
-          this.classList.remove("active");
-          gsap.set(this, {
-            height: "30%",
-            clearProps: "all"
-          });
-        } else {
-          for (let i = 0; i < notes.length; i++) {
-            if (notes[i].classList.contains("active")) {
-              notes[i].classList.remove("active");
-              gsap.set(notes[i], {
-                height: "30%",
-                clearProps: "all"
-              });
-            }
-          }
-          this.classList.add("active");
-          gsap.set(this, {
-            height: 125 + 40 * i + "%"
-          });
-        }
-      } else if (tablet_media_query.matches) {
-        if (this.classList.contains("active")) {
-          this.classList.remove("active");
-          gsap.set(this, {
-            height: "30%",
-            clearProps: "all"
-          });
-        } else {
-          for (let i = 0; i < notes.length; i++) {
-            if (notes[i].classList.contains("active")) {
-              notes[i].classList.remove("active");
-              gsap.set(notes[i], {
-                height: "30%",
-                clearProps: "all"
-              });
-            }
-          }
-          this.classList.add("active");
-          gsap.set(this, {
-            height: 80 + 21 * i + "%"
-          });
-        }
-      } else {
-        if (this.classList.contains("active")) {
-          this.classList.remove("active");
-          gsap.set(this, {
-            height: "30%",
-            clearProps: "all"
-          });
-        } else {
-          for (let i = 0; i < notes.length; i++) {
-            if (notes[i].classList.contains("active")) {
-              notes[i].classList.remove("active");
-              gsap.set(notes[i], {
-                height: "30%",
-                clearProps: "all"
-              });
-            }
-          }
-          this.classList.add("active");
-          gsap.set(this, {
-            height: 70 + 20 * i + "%"
-          });
-        }
+let highestZ = 1;
+class Paper {
+  holdingPaper = false;
+  mouseTouchX = 0;
+  mouseTouchY = 0;
+  mouseX = 0;
+  mouseY = 0;
+  prevMouseX = 0;
+  prevMouseY = 0;
+  velX = 0;
+  velY = 0;
+  rotation = Math.random() * 30 - 15;
+  currentPaperX = 0;
+  currentPaperY = 0;
+  rotating = false;
+  init(paper) {
+    document.addEventListener('mousemove', (e) => {
+      if(!this.rotating) {
+        this.mouseX = e.clientX;
+        this.mouseY = e.clientY;
+        this.velX = this.mouseX - this.prevMouseX;
+        this.velY = this.mouseY - this.prevMouseY;
       }
+      const dirX = e.clientX - this.mouseTouchX;
+      const dirY = e.clientY - this.mouseTouchY;
+      const dirLength = Math.sqrt(dirX*dirX+dirY*dirY);
+      const dirNormalizedX = dirX / dirLength;
+      const dirNormalizedY = dirY / dirLength;
+      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
+      let degrees = 180 * angle / Math.PI;
+      degrees = (360 + Math.round(degrees)) % 360;
+      if(this.rotating) {
+        this.rotation = degrees;
+      }
+      if(this.holdingPaper) {
+        if(!this.rotating) {
+          this.currentPaperX += this.velX;
+          this.currentPaperY += this.velY;
+        }
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
+        paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
+      }
+    })
+    paper.addEventListener('mousedown', (e) => {
+      if(this.holdingPaper) return;
+      this.holdingPaper = true;
+      paper.style.zIndex = highestZ;
+      highestZ += 1;
+      if(e.button === 0) {
+        this.mouseTouchX = this.mouseX;
+        this.mouseTouchY = this.mouseY;
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
+      }
+      if(e.button === 2) {
+        this.rotating = true;
+      }
+    });
+    window.addEventListener('mouseup', () => {
+      this.holdingPaper = false;
+      this.rotating = false;
     });
   }
 }
-
-//-> Function that set up the up paper of the envelope.
-function set_up_paper() {
-  var arr = [0, 0, 100, 0, 50, 61];
-  gsap.set(".js-up-paper", {
-    bottom: "97%",
-    rotation: 180,
-    zIndex: 200,
-    clipPath:
-      "polygon(" +
-      arr[0] +
-      "%" +
-      arr[1] +
-      "%," +
-      arr[2] +
-      "%" +
-      arr[3] +
-      "%," +
-      arr[4] +
-      "%" +
-      arr[5] +
-      "%)",
-    onComplete: notes_ready
-  });
-}
-
-//-> Function that starts the up paper transition.
-function envelop_transition() {
-  gsap.to(".js-up-paper", {
-    bottom: "1%",
-    duration: 0.25,
-    onComplete: set_up_paper
-  });
-  document
-    .querySelector(".js-up-paper")
-    .removeEventListener("click", envelop_transition);
-  document.querySelector(".js-up-paper").classList.remove("cursor");
-}
-
-//-> Function that allows cut the sticker.
-function sticker() {
-  gsap.set(".js-sticker", { width: "20%", left: "-80%" });
-  document.body.classList.remove("scissors");
-  document.querySelector(".js-sticker").removeEventListener("click", sticker);
-  document
-    .querySelector(".js-up-paper")
-    .addEventListener("click", envelop_transition);
-  document.querySelector(".js-up-paper").classList.add("cursor");
-}
-
-document.querySelector(".js-sticker").addEventListener("click", sticker);
-
-window.onresize = function (event) {
-  recize_notes();
-};
+const papers = Array.from(document.querySelectorAll('.paper'));
+papers.forEach(paper => {
+  const p = new Paper();
+  p.init(paper);
+});
